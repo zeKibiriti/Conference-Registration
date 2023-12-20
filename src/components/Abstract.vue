9a9df6a59b286dcdb790b969706a9f584dbba689
Ngallya <tkibiriti@gmail.com>
2023-12-20 16:40:22 +0300

    Adding abstract form

--- src/components/Abstract.vue
+++ src/components/Abstract.vue
@@ -19,121 +19,141 @@
                 <v-container fluid fill-height>
                   <!-- Select Category -->
                   <v-row align="center" justify="center">
-                    <v-col cols="12" md="4">
+                    <v-col cols="12" md="6">
+                      <v-text-field
+                        v-model="email"
+                        :rules="emailRules"
+                        label="Valid E-mail"
+                        hide-details
+                        required
+                      ></v-text-field>
+                    </v-col>
+
+                    <v-col cols="12" md="6">
+                      <v-text-field
+                        v-model="title"
+                        :rules="nameRules"
+                        :counter="10"
+                        label="Title"
+                        required
+                        hide-details
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="12">
                       <v-select
-                        v-model="category"
-                        :items="['Student', 'Non Student']"
-                        label="Select Category"
+                        v-model="subTheme"
+                        :items="['Nutrition and NCDs', 'NCD Emergency preparedness']"
+                        label="Select Conference Sub-Theme"
                       >
                         <template #append-outer>
                           <v-icon @click="clearSelection">mdi-close-circle</v-icon>
                         </template>
                       </v-select>
                     </v-col>
+                  </v-row>
 
-                    <v-col cols="12" md="4">
-                      <v-select
-                        v-model="country"
-                        :items="['Tanzania', 'Kenya', 'Uganda']"
-                        label="Select Country"
-                      >
-                        <template #append-outer>
-                          <v-icon @click="clearSelection">mdi-close-circle</v-icon>
-                        </template>
-                      </v-select>
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="12">
+                        <v-text-field
+                          v-model="authors"
+                          :rules="nameRules"
+                          :counter="200"
+                          label="Authors"
+                          required
+                          hide-details
+                        ></v-text-field>
+                      </v-col>
+                  </v-row>
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="6">
+                      <v-text-field
+                        v-model="affiliation"
+                        :rules="nameRules"
+                        :counter="200"
+                        label="Affiliation"
+                        required
+                        hide-details
+                      ></v-text-field>
                     </v-col>
 
-                    <v-col cols="12" md="4">
-                      <v-select
-                        v-model="gender"
-                        :items="['Male', 'Female']"
-                        label="Select Gender"
-                      >
-                        <template #append-outer>
-                          <v-icon @click="clearSelection">mdi-close-circle</v-icon>
-                        </template>
-                      </v-select>
+                    <v-col cols="12" md="6">
+                      <v-text-field
+                        v-model="presentingAuthor"
+                        :rules="nameRules"
+                        :counter="200"
+                        label="Presenting author"
+                        required
+                        hide-details
+                      ></v-text-field>
                     </v-col>
                   </v-row>
                   <!-- Name Fields -->
                   <v-row align="center" justify="center">
-                    <v-col cols="12" md="4">
-                      <v-text-field
-                        v-model="firstname"
-                        :rules="nameRules"
-                        :counter="10"
-                        label="First name"
-                        required
-                        hide-details
-                      ></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="4">
-                      <v-text-field
-                        v-model="middlename"
-                        :rules="nameRules"
-                        :counter="10"
-                        label="Middle name"
-                        required
-                        hide-details
-                      ></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="4">
-                      <v-text-field
-                        v-model="lastname"
-                        :rules="nameRules"
-                        :counter="10"
-                        label="Last name"
-                        hide-details
-                        required
-                      ></v-text-field>
-                    </v-col>
-                  </v-row>
-
-                  <!-- Email and Phone Number Fields -->
-                  <v-row align="center" justify="center">
-                    <v-col cols="12" md="4">
-                      <v-text-field
-                        v-model="email"
-                        :rules="emailRules"
-                        label="E-mail"
-                        hide-details
-                        required
-                      ></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="4">
-                      <v-text-field
-                        v-model="password"
-                        :rules="passwordRules"
-                        label="Password"
-                        hide-details
-                        required
-                        type="password"
-                      ></v-text-field>
-                    </v-col>
-
-                    <v-col cols="12" md="4">
-                      <v-text-field
-                        v-model="phoneNumber"
-                        :rules="phoneRules"
-                        label="Phone Number"
-                        @input="formatPhoneNumber"
-                        hide-details
-                        required
-                      ></v-text-field>
+                    <v-col cols="12" md="12">
+                      <v-textarea
+                        v-model="background"
+                        :counter="50"
+                        label="Background"
+                        placeholder="Enter your background here, maximum 50 words..."
+                      ></v-textarea>
                     </v-col>
                   </v-row>
 
                   <!-- Description Field -->
                   <v-row align="center" justify="center">
                     <v-col cols="6" md="12">
+                      <v-text-field
+                        v-model="objective"
+                        :counter="50"
+                        label="Objective/Aims"
+                        placeholder="Enter your Objective/Aims here..."
+                      ></v-text-field>
+                    </v-col>
+                  </v-row>
+
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="12">
                       <v-textarea
-                        v-model="description"
-                        :counter="300"
-                        label="Description"
-                        placeholder="Enter your description here..."
+                        v-model="methodology"
+                        :counter="75"
+                        label="Methodology/Project plan"
+                        placeholder="Enter your Methodology/Project plan here, maximum 75 words..."
+                      ></v-textarea>
+                    </v-col>
+                  </v-row>
+
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="12">
+                      <v-textarea
+                        v-model="results"
+                        :counter="130"
+                        label="Results/Progress"
+                        placeholder="Enter your Results/Progress here, maximum 130 words..."
+                      ></v-textarea>
+                    </v-col>
+                  </v-row>
+
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="12">
+                      <v-textarea
+                        v-model="conclusion"
+                        :counter="15"
+                        label="Conclusion/Lessons learned"
+                        placeholder="Enter your Results/Progress here, maximum 15 words..."
+                      ></v-textarea>
+                    </v-col>
+                  </v-row>
+
+                  <v-row align="center" justify="center">
+                    <v-col cols="12" md="12">
+                      <v-textarea
+                        v-model="recommendations"
+                        :counter="15"
+                        label="Recommendations"
+                        placeholder="Enter your Recommendations here, maximum 15 words..."
                       ></v-textarea>
                     </v-col>
                   </v-row>
@@ -142,7 +162,7 @@
                   <v-row class="mt-4">
                     <!-- Cancel Button -->
                     <v-col>
-                      <v-btn @click="closeDialog('dialog1')">Cancel</v-btn>
+                      <v-btn @click="cancelForm">Cancel</v-btn>
                     </v-col>
                     <!-- Reset Button -->
                     <v-col>
@@ -156,144 +176,6 @@
                 </v-container>
               </v-form>
             </v-card>
-<!--&lt;!&ndash;              <h3 style="text-align: center; margin-top: 20px">Conference Registration Form</h3>&ndash;&gt;-->
-<!--              &lt;!&ndash; Form Content &ndash;&gt;-->
-<!--              <v-form v-model="valid" @submit.prevent="submitForm">-->
-<!--                <v-container fluid fill-height>-->
-<!--                  &lt;!&ndash; Select Category &ndash;&gt;-->
-<!--                  <v-row align="center" justify="center">-->
-<!--                    <v-col cols="12" md="6">-->
-<!--                      <v-select-->
-<!--                        v-model="selectedItem"-->
-<!--                        :items="['Student', 'Non Student']"-->
-<!--                        label="Select Category"-->
-<!--                      >-->
-<!--                        <template #append-outer>-->
-<!--                          <v-icon @click="clearSelection">mdi-close-circle</v-icon>-->
-<!--                        </template>-->
-<!--                      </v-select>-->
-<!--                    </v-col>-->
-
-<!--                    <v-col cols="12" md="6">-->
-<!--                      <v-select-->
-<!--                        v-model="selectedItem"-->
-<!--                        :items="['Tanzania', 'Kenya', 'Uganda']"-->
-<!--                        label="Select Country"-->
-<!--                      >-->
-<!--                        <template #append-outer>-->
-<!--                          <v-icon @click="clearSelection">mdi-close-circle</v-icon>-->
-<!--                        </template>-->
-<!--                      </v-select>-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-<!--                  <v-row>-->
-<!--                    <h4>Gender</h4>-->
-<!--                    <hr>-->
-<!--                    <v-radio-group-->
-<!--                      v-model="inline"-->
-<!--                      inline-->
-<!--                    >-->
-<!--                      <v-radio-->
-<!--                        label="Male"-->
-<!--                        value="Male"-->
-<!--                      ></v-radio>-->
-<!--                      <v-radio-->
-<!--                        label="Female"-->
-<!--                        value="Female"-->
-<!--                      ></v-radio>-->
-<!--                    </v-radio-group>-->
-<!--                  </v-row>-->
-
-<!--                  &lt;!&ndash; Name Fields &ndash;&gt;-->
-<!--                  <v-row align="center" justify="center">-->
-<!--                    <v-col cols="12" md="4">-->
-<!--                      <v-text-field-->
-<!--                        v-model="firstname"-->
-<!--                        :rules="nameRules"-->
-<!--                        :counter="10"-->
-<!--                        label="First name"-->
-<!--                        required-->
-<!--                        hide-details-->
-<!--                      ></v-text-field>-->
-<!--                    </v-col>-->
-
-<!--                    <v-col cols="12" md="4">-->
-<!--                      <v-text-field-->
-<!--                        v-model="middlename"-->
-<!--                        :rules="nameRules"-->
-<!--                        :counter="10"-->
-<!--                        label="Middle name"-->
-<!--                        required-->
-<!--                        hide-details-->
-<!--                      ></v-text-field>-->
-<!--                    </v-col>-->
-
-<!--                    <v-col cols="12" md="4">-->
-<!--                      <v-text-field-->
-<!--                        v-model="lastname"-->
-<!--                        :rules="nameRules"-->
-<!--                        :counter="10"-->
-<!--                        label="Last name"-->
-<!--                        hide-details-->
-<!--                        required-->
-<!--                      ></v-text-field>-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-
-<!--                  &lt;!&ndash; Email and Phone Number Fields &ndash;&gt;-->
-<!--                  <v-row align="center" justify="center">-->
-<!--                    <v-col cols="12" md="6">-->
-<!--                      <v-text-field-->
-<!--                        v-model="email"-->
-<!--                        :rules="emailRules"-->
-<!--                        label="E-mail"-->
-<!--                        hide-details-->
-<!--                        required-->
-<!--                      ></v-text-field>-->
-<!--                    </v-col>-->
-
-<!--                    <v-col cols="12" md="6">-->
-<!--                      <v-text-field-->
-<!--                        v-model="phoneNumber"-->
-<!--                        :rules="phoneRules"-->
-<!--                        label="Phone Number"-->
-<!--                        @input="formatPhoneNumber"-->
-<!--                        hide-details-->
-<!--                        required-->
-<!--                      ></v-text-field>-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-
-<!--                  &lt;!&ndash; Description Field &ndash;&gt;-->
-<!--                  <v-row align="center" justify="center">-->
-<!--                    <v-col cols="12" md="12">-->
-<!--                      <v-textarea-->
-<!--                        v-model="description"-->
-<!--                        :counter="300"-->
-<!--                        label="Description"-->
-<!--                        placeholder="Enter your description here..."-->
-<!--                      ></v-textarea>-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-
-<!--                  &lt;!&ndash; Buttons &ndash;&gt;-->
-<!--                  <v-row class="mt-4">-->
-<!--                    &lt;!&ndash; Cancel Button &ndash;&gt;-->
-<!--                    <v-col>-->
-<!--                      <v-btn @click="cancelForm">Cancel</v-btn>-->
-<!--                    </v-col>-->
-<!--                    &lt;!&ndash; Reset Button &ndash;&gt;-->
-<!--                    <v-col>-->
-<!--                      <v-btn @click="resetForm" color="error">Reset</v-btn>-->
-<!--                    </v-col>-->
-<!--                    &lt;!&ndash; Submit Button &ndash;&gt;-->
-<!--                    <v-col>-->
-<!--                      <v-btn type="submit" color="primary">Submit</v-btn>-->
-<!--                    </v-col>-->
-<!--                  </v-row>-->
-<!--                </v-container>-->
-<!--              </v-form>-->
-<!--            </v-card>-->
           </v-col>
         </v-row>
       </v-col>
@@ -321,6 +203,17 @@ export default {
       middlename: "",
       lastname: "",
       email: "",
+      title: "",
+      authors: "",
+      presentingAuthor: "",
+      subTheme: "",
+      affiliation: "",
+      background: "",
+      objective: "",
+      methodology: "",
+      results: "",
+      conclusion: "",
+      recommendations: "",
       password: "",
       passwordRules: [
         (v) => !!v || "Password is required",
@@ -359,6 +252,42 @@ export default {
     },
     handleButtonClick1() {
       // Handle the first button click
+    },
+    resetForm() {
+      // Reset form fields
+      this.country = "";
+      this.category = "";
+      this.gender = "";
+      this.firstname = "";
+      this.middlename = "";
+      this.lastname = "";
+      this.email = "";
+      this.password = "";
+      this.phoneNumber = "";
+      this.authors = "";
+      this.title = "";
+      this.affiliation = "";
+      this.description = "";
+      this.objective = "";
+      this.methodology = "";
+      this.results = "";
+      this.conclusion = "";
+      this.recommendations = "";
+      // Reset validation state if using validation
+      this.$refs.submitForm?.resetValidation(); // Replace "form" with the ref attribute of your form element
+    },
+    clearSelection() {
+      this.category = null;
+    },
+    cancelForm() {
+      this.$router.push({ path: "/" });
+      // this.dialog = false;
+    },
+    formatPhoneNumber() {
+      // Handle phone number formatting logic
+    },
+    submitForm() {
+      // Your form submission logic here
     },
   },
 };
